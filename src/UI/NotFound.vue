c0b9bd71b1b981ea81a698277ecb47f64c16f98a
Hiren Kuriya <hirenkuriya7089@gmail.com>
2024-05-19 18:35:25 +0530

    restrict unwanted routes and add 404 page

---
+++ src/UI/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-center">
+    <h2 style="text-align: center">No page found!</h2>
+    <h3>
+      Do you want to <router-link to="/step-1">Start again</router-link>...?
+    </h3>
+  </div>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import store from "./store.js";
 import StepOne from "./components/StepOne.vue";
 import StepTwo from "./components/StepTwo.vue";
 import StepThree from "./components/StepThree.vue";
+import NotFound from "./UI/NotFound.vue";
 
 const routers = createRouter({
   history: createWebHistory(),
@@ -16,6 +17,7 @@ const routers = createRouter({
       component: StepThree,
       meta: { requireData: true },
     },
+    { path: "/:404(.*)", component: NotFound },
   ],
 });
 
